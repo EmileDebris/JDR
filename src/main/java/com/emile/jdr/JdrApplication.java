710ec0fb96978779cbcfd33af9a6bdea3b9cba1b
Emile DEBRIS <emile.gd@orange.fr>
2023-04-30 16:40:53 +0200

    INIT

---
+++ src/main/java/com/emile/jdr/JdrApplication.java
@@ -0,0 +1,13 @@
+package com.emile.jdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emile/jdr/JdrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emile.jdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
